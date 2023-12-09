fcbd8e299ec2de1285302ef7eec133e988113f8a
unknown <amal.ramzy2006@gmail.com>
2023-12-09 22:33:53 +0200

    second commit

--- main.cpp
+++ main.cpp
@@ -11,23 +11,29 @@ int main() {
 	int to,from;
 	CurrencyList l1;
 	CurrencyList l2;
-	l1.insertEnd(1, "Yen",15.0);
- 	l1.insertEnd(2, "Bitcoin",200.0);
-	l2.insertEnd(1,"Bitcoin",200.2);
+	l1.insertEnd(1, "EGP",128859.74);
+ 	l1.insertEnd(2, "USD",41675.20);
+ 	l1.insertEnd(3, "SAR",156329.63);
+ 	l1.insertEnd(4, "JPY",614167.22);
+ 	l1.insertEnd(5, "EUR",38469.86);
+	l2.insertEnd(1,"Bitcoin",128859.74);
+	l2.insertEnd(2,"Bitcoin",41675.20);
+	l2.insertEnd(3,"Bitcoin",156329.63);
+	l2.insertEnd(4,"Bitcoin",614167.22);
+	l2.insertEnd(5,"Bitcoin",38469.86);
 	cout << "------------------- Welcome ------------" << endl;
 	cout << "----------------------------------------" << endl;
 	l1.traverse();
 	cout << "Select Currency by its number :";
 	
 	cin >> from;
-	cout << from + " selected" << endl;
-	l2.traverse();
-	cout << "Select the Currency You Want to Convert To : " ;
 	
-	cin >> to;
+	cout << "----------------------------------------" << endl;
+
+
 	cout << endl;
 
-	if(l2.search(to)){
+	if(l2.search(from)){
 		int i  = 0; // key
 		string k;  // name
 		double p;//price
@@ -35,7 +41,7 @@ int main() {
 	
 l2.retrieveData(p,k,i);
 cout << "the Currency with Key: " << i << " with the name " << k << " is ..Drum roll.. equal : " << p << endl;
-l2.toFirst();
+
 	}
 
 	return 0;
